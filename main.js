ef6a0dd70522c174effd80d2bc4ee161740972ef
annayyada <sindhubangaru123@gmail.com>
2018-03-29 13:48:35 +0530

    added json and js

---
+++ main.js
@@ -0,0 +1,93 @@
+// Ajax call(XMLHttpRequest) initiation
+function loadjson(file,callback) {
+  var xhr=new XMLHttpRequest();
+  xhr.overrideMimeType("application/json");
+  xhr.open("GET",file,true);
+  xhr.onreadystatechange=function () {
+    if(xhr.readyState ===4 && xhr.status =="200"){
+      callback(xhr.responseText);
+    }
+  }
+xhr.send();
+}
+// function calling
+loadjson("data.json", function(text){
+  let data=JSON.parse(text);
+  console.log(data);
+  basics(data.left);
+  education(data.education);
+  skill(data.skills);
+})
+// for main div class calling
+// var main=document.queryselector('.main');
+// element creation
+var main=document.getElementById('main');
+var left=document.createElement("div");
+// add class name to element
+left.classList.add("left");
+   // add id to element
+//left.setAttribute("id","left");
+ // adding text to div
+left.textContent="Profile Details:"
+left.appendChild(document.createElement("HR"));
+  // appending to main div
+main.appendChild(left);
+function basics(leftside) {
+var image=document.createElement("img");
+image.src=leftside.photo;
+left.appendChild(image);
+var name=document.createElement("h1");
+name.textContent=leftside.name;
+left.appendChild(name);
+var email=document.createElement("h2");
+email.textContent=leftside.email;
+name.appendChild(email);
+var ph=document.createElement("p");
+ph.textContent=leftside.phone;
+email.appendChild(ph);
+console.log(left);
+}
+
+// right div creation
+var right=document.createElement("div");
+right.classList.add("right");
+main.appendChild(right);
+// education div setAttribute
+var edu=document.createElement("div");
+edu.classList.add("edu1");
+edu.textContent="Education Details:";
+edu.appendChild(document.createElement("HR"));
+right.appendChild(edu);
+function education(educa) {
+  for(i in educa){
+    var e1=document.createElement("h1");
+    e1.classList.add("edu2");
+    e1.textContent=educa[i].course;
+
+    var ul=document.createElement("ul");
+    for(j in educa[i].college) {
+      var li=document.createElement("li");
+      li.textContent=educa[i].college[j];
+      ul.appendChild(li);
+      e1.appendChild(ul);
+      edu.appendChild(e1);
+
+      }
+  }
+}
+function skill(skills){
+  var skill_title=document.createElement("h1");
+  skill_title.classList.add("tech_skills");
+  skill_title.textContent="Technical skills:";
+  skill_title.appendChild(document.createElement("HR"));
+  right.appendChild(skill_title);
+  // table creation
+  var table=document.createElement("table");
+  table.setAttribute("id","tab");
+  var row="";
+  for(var i = 0; i < skills.length; i++){
+    row =row+"<tr><td>"+skills[i].name+"</td><td>"+skills[i].value+"</td></tr>"
+  }
+  table.innerHTML=row;
+  skill_title.appendChild(table);
+}
